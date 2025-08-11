903ccda5bc682da52a86fadf2304502faa6cbbc0
Ahmed Ezzat <ahmed.ezzat0407@gmail.com>
2025-08-12 00:25:38 +0300

    creat edit note view

--- lib/views/widgets/custom_NoteItem.dart
+++ lib/views/widgets/custom_NoteItem.dart
@@ -1,61 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key, required this.noteColor});
   final noteColor;
   @override
-  Widget build(Object context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: noteColor,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(left: 20, bottom: 12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text(
-                  'Flutter Tips',
-                  style: TextStyle(color: Colors.black, fontSize: 26),
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return EditNoteView();
+        }));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: noteColor,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, bottom: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: const Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text(
+                    'Flutter Tips',
+                    style: TextStyle(color: Colors.black, fontSize: 26),
+                  ),
                 ),
-              ),
-              trailing: IconButton(
-                padding: EdgeInsets.only(bottom: 50),
-                onPressed: () {},
-                icon: Icon(
-                  FontAwesomeIcons.trash,
+                trailing: IconButton(
+                  padding: const EdgeInsets.only(bottom: 50),
+                  onPressed: () {},
+                  icon: const Icon(
+                    FontAwesomeIcons.trash,
+                  ),
+                  color: Colors.black,
+                  iconSize: 26,
                 ),
-                color: Colors.black,
-                iconSize: 26,
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(
-                  top: 16,
-                ),
-                child: Text(
-                  'Build your career with hard work',
-                  style: TextStyle(
-                    color: Colors.black.withAlpha((255 * .5).toInt()),
-                    fontSize: 18,
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 16,
+                  ),
+                  child: Text(
+                    'Build your career with hard work',
+                    style: TextStyle(
+                      color: Colors.black.withAlpha((255 * .5).toInt()),
+                      fontSize: 18,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 24, top: 14),
-              child: Text(
-                'August 9, 2025',
-                style: TextStyle(
-                    color: Colors.black.withAlpha((255 * .5).toInt()),
-                    fontSize: 16),
-              ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(right: 24, top: 14),
+                child: Text(
+                  'August 9, 2025',
+                  style: TextStyle(
+                      color: Colors.black.withAlpha((255 * .5).toInt()),
+                      fontSize: 16),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/views/widgets/edit_note_veiw_body.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class EditNoteVeiwBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
